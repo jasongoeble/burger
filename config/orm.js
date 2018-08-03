d6ee843c2246b95cf80a57f98cd946cb443c66c5
Jason Goeble <jason.goeble@gmail.com>
2018-08-03 11:50:52 -0400

    stuck

--- config/orm.js
+++ config/orm.js
@@ -1,107 +1,42 @@
-// Import MySQL connection.
+//dependencies
 var connection = require("../config/connection.js");
 
-// Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
-function printQuestionMarks(num) {
-  var arr = [];
-
-  for (var i = 0; i < num; i++) {
-    arr.push("?");
-  }
-
-  return arr.toString();
-}
-
-// Helper function to convert object key/value pairs to SQL syntax
-function objToSql(ob) {
-  var arr = [];
-
-  // loop through the keys and push the key/value as a string int arr
-  for (var key in ob) {
-    var value = ob[key];
-    // check to skip hidden properties
-    if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-      if (typeof value === "string" && value.indexOf(" ") >= 0) {
-        value = "'" + value + "'";
-      }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
-      arr.push(key + "=" + value);
+//define the object relational mapper
+var orm = 
+{
+    //select all query - all values from the specified table (tableInput)
+    all: function(tableInput) 
+    {
+        var queryString = "SELECT * FROM ??";
+        connection.query(queryString, [tableInput], function(err, result) 
+        {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    //insert query - insert a rows worth of data (newRow) into the specified table (tableInput)
+    create: function(tableInput, newRow) 
+    {
+        var queryString = "INSERT INTO ?? SET ??";
+        console.log(queryString);
+        connection.query(queryString, [tableInput, newRow], function(err, result) 
+        {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    //update query - update the value of fields in a specified column (colName) to a new value (newValue) in the specified table (tableInput) 
+    //only where a specific column value (targetCol) is equal to the desired condition (targetValue)
+    update: function(tableInput, colName, newValue, targetCol, targetValue) 
+    {
+        var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+        console.log(queryString);
+        connection.query(queryString,[tableInput, colName, newValue, targetCol, targetValue], function(err, result) 
+        {
+            if (err) throw err;
+            console.log(result);
+        });
     }
-  }
-
-  // translate array of strings to a single comma-separated string
-  return arr.toString();
-}
-
-// Object for all our SQL statement functions.
-var orm = {
-  all: function(tableInput, cb) {
-    var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-      cb(result);
-    });
-  },
-  create: function(table, cols, vals, cb) {
-    var queryString = "INSERT INTO " + table;
-
-    queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
-    queryString += ") ";
-
-    console.log(queryString);
-
-    connection.query(queryString, vals, function(err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  },
-  // An example of objColVals would be {name: panther, sleepy: true}
-  update: function(table, objColVals, condition, cb) {
-    var queryString = "UPDATE " + table;
-
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += condition;
-
-    console.log(queryString);
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  },
-  delete: function(table, condition, cb) {
-    var queryString = "DELETE FROM " + table;
-    queryString += " WHERE ";
-    queryString += condition;
-
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  }
 };
-
-// Export the orm object for the model (burger.js).
-module.exports = orm;
+  
+  module.exports = orm;
